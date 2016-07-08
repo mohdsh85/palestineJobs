6c18da1b244a41d76988e8b94561b45c053fb4be
mohdsh85 <mohdsh85@gmail.com>
2016-07-08 03:09:54 +0300

    Add files via upload

---
+++ countries.php
@@ -0,0 +1,21 @@
+<?
+    $array=array(
+        array(
+        'name'=>'Bahrain',
+        'name_ar'=>'البحرين',
+        'geo'=>'bh'
+        ),
+        array(
+            'name'=>'Egypt',
+            'name_ar'=>'مصر',
+            'geo'=>'eg'
+        ),
+        array(
+            'name'=>'Jordan',
+            'name_ar'=>'الأردن',
+            'geo'=>'jo'
+        ),
+    );
+
+echo json_encode($array);
+?>
